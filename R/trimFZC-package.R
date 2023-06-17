2271491dabcfe494a4680030c5c3329fdc3d7b3d
maxchen_MacBook <maxchenyuling@gmail.com>
2023-06-17 16:19:36 -0400

    added vignette

--- R/trimFZC-package.R
+++ R/trimFZC-package.R
@@ -3,6 +3,20 @@
 #' @name trimFZC-package
 #' @aliases trimFZC
 #' @description
-#' This is the
+#' 'trimFZC' package contains 3 main functions: \code{getTrimMean()}, \code{plotter}, and \code{tabler}.
+#' \itemize{
+#'  \item \code{getTrimMean()}: generates trimmed mean estimates of simulated samples from Normal
+#'  distribution, see \link{getTrimMean} for details.
+#'  \item \code{plotter}: draws various plots for analysis of the efficiency of the trimmed
+#'  mean estimates, see \link{plotter} for details.
+#'  \item \code{tabler}: creates tabularized statistical summaries of the trimmed
+#'  mean estimates, see \link{tabler} for details.
+#' }
+#' For demonstrative examples, see \code{vignette("vignette")}.
+#'
+#'
+#' @author Yuling Max Chen (\email{yuling.chen@uwaterloo.ca}, developer)
+#' @author Yuxuan Zhao (contributor)
+#' @author Megan French (contributor)
 #'
 NULL
